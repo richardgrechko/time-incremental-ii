f29d790de404c9dbfd12b86e03d8bdf7b7f7f10d
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-15 15:15:20 +0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,12 @@
 function upgradeUI(up) {
 	document.querySelector(`div#${up}.current-gain`).textContent = `${format(data.upgrades[up].getMulti())} (${format(data.upgrades[up].level)})`
-	document.querySelector(`div#${up}.upg-button`).textContent = formatTimeEx(data.upgrades[up].getCost())
+	document.querySelector(`button#${up}.upg-button`).textContent = formatTimeEx(data.upgrades[up].getCost())
+	document.querySelector(`button#${up}.upg-button`).onclick = (_ => {
+		data.upgrades[up].buy()
+	})
+	document.querySelector(`button#${up}.upg-max-button`).onclick = (_ => {
+		data.upgrades[up].buyMax()
+	})
 }
 function updateUI() {
 	document.getElementById("points-stat").textContent = formatTimeEx(data.seconds)
