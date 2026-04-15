85d6a7369dd64ccb133f33264db5d43996fab4cd
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-15 13:18:33 +0500

    Update data.js

--- configs/data.js
+++ configs/data.js
@@ -1,7 +1,22 @@
 const data = {
 	seconds: new Decimal(0),
-	speed: new Decimal(1e-30), // (xNUM speed means you get +NUM seconds per second)
+	speed: new Decimal(1e-30), // xNUM speed means you get +NUM seconds per second
 	upgrades: {
-		
-	}
+		hastener: new Upgrade({
+			cost: new Decimal(1e-29),
+			multi: new Decimal(2.1),
+			gain: new Decimal(2)
+		}),
+		generator: new Upgrade({
+			cost: new Decimal(1e-12),
+			multi: new Decimal(10),
+			gain: new Decimal(2)
+		}),
+		powerer: new Upgrade({
+			cost: new Decimal(31556952),
+			multi: new Decimal(25),
+			gain: new Decimal(4)
+		}),
+	},
+	timeParadoxes: new Decimal(0),
 }
