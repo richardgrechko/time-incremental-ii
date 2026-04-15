d4b01e47b3c124c82b92d8250ccd8eb1099c2ba2
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-15 18:22:09 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 funcs.save();
 funcs.load();
-(function main(){
-  dt1 = Date.now()
-  funcs.update((dt1-dt2)/1000)
-  dt2 = Date.now()
-  updateUI()
-  requestAnimationFrame(main)
-})()
+setInterval(updateUI,50)
+funcs.update()
