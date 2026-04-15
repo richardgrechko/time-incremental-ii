b02ffab8606a7cc18d04582efb4926d146a83dbb
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-15 17:05:59 +0500

    Update functions.js

--- configs/functions.js
+++ configs/functions.js
@@ -1,3 +1,6 @@
+let delta = 0
+let dt1 = Date.now();
+let dt2 = Date.now();
 const funcs = {
 	save() {
 		localStorage.setItem("timeincrementaliisave",btoa(unescape(encodeURIComponent(JSON.stringify(data)))))
@@ -8,33 +11,40 @@ const funcs = {
 	load() {
 		let obj = funcs.getSave()
 		try {
-			data.seconds = fixValue(obj.seconds);
-			data.speed = fixValue(obj.speed,1e-30);
+			data.seconds = fixValue(new Decimal(obj.seconds));
+			data.speed = fixValue(new Decimal(obj.speed),1e-30);
 			data.upgrades = {};
-			data.upgrades?.hastener = obj.upgrades?.hastener ?? new Upgrade({
+			data.upgrades.hastener = new Upgrade(obj.upgrades.hastener ?? {
 				cost: new Decimal(1e-29),
-				multi: new Decimal(2.1),
-				gain: new Decimal(2)
+				multi: new Decimal(2),
+				gain: new Decimal(1.5)
 			});
-			data.upgrades?.generator = obj.upgrades?.generator ?? new Upgrade({
+			data.upgrades.hastener.level = new Decimal(obj.upgrades.hastener.level)
+			data.upgrades.generator = new Upgrade(obj.upgrades.generator ?? {
 				cost: new Decimal(1e-12),
 				multi: new Decimal(10),
 				gain: new Decimal(2)
 			});
-			data.upgrades?.powerer = obj.upgrades?.powerer ?? new Upgrade({
+			data.upgrades.generator.level = new Decimal(obj.upgrades.generator.level)
+			data.upgrades.powerer = new Upgrade(obj.upgrades.powerer ?? {
 				cost: new Decimal(31556952),
 				multi: new Decimal(25),
 				gain: new Decimal(4)
 			});
+			data.upgrades.powerer.level = new Decimal(obj.upgrades.powerer.level)
 		} catch (e) {
 			alert(e)
 		}
 	},
-	update(delta) {
-		data.seconds = data.seconds.add(data.speed.div(delta))
+	update() {
+		dt1 = Date.now()
+		delta = (dt1-dt2)/1000
+		dt2 = Date.now()
+		data.seconds = data.seconds.add(data.speed.mul(delta))
 		data.speed = data.upgrades.hastener.getMulti()
 		.mul(data.upgrades.generator.getMulti())
 		.mul(data.upgrades.powerer.getMulti())
-		.div(1e33)
+		.div(1e30)
+		requestAnimationFrame(funcs.update)
 	},
 }
