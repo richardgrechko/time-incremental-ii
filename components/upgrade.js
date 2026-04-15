2948532a2c861251ba72014a751696b1e01f680a
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-15 15:16:18 +0500

    Update upgrade.js

--- components/upgrade.js
+++ components/upgrade.js
@@ -1,32 +1,36 @@
 class Upgrade {
 	constructor(opt) {
 		this.level = new Decimal(0);
-		this.cost = opt.cost ?? new Decimal(1);
-		this.multi = opt.multi ?? new Decimal(1);
-		this.gain = opt.gain ?? new Decimal(1);
+		this.cost = new Decimal(opt.cost) ?? new Decimal(1);
+		this.multi = new Decimal(opt.multi) ?? new Decimal(1);
+		this.gain = new Decimal(opt.gain) ?? new Decimal(1);
 	}
 	getMulti() {
-		if (this.gain.lt(new Decimal(2).pow(128))) return this.gain
-		return this.gain.root(this.multi.log(new Decimal(2).pow(128)).root(1.5))
+		if (this.gain.lt(new Decimal(2).pow(128))) return this.gain.pow(this.level)
+		return this.gain.pow(this.level).root(this.gain.pow(this.level).log(new Decimal(2).pow(128)).root(1.5))
 	}
 	getCost() {
 		return this.cost.mul(this.multi.pow(this.level))
 	}
 	buy() {
 		if (data.seconds.gt(this.getCost())) {
-			this.level = this.level.add(1)
 			data.seconds = data.seconds.sub(this.getCost())
+			this.level = this.level.add(1)
 		}
 	}
 	buyMax() {
 		if (data.seconds.gt(this.getCost())) {
-			this.level = this.level.add(
-				Decimal.affordGeometricSeries(data.seconds,this.getCost(),this.multi)
-			)
-			data.seconds = data.seconds.sub(
+			let prev_seconds = data.seconds
+			data.seconds = 
+				Decimal.affordGeometricSeries(data.seconds,this.getCost(),this.multi).eq(1)?
+				data.seconds.sub(this.getCost()):
+				data.seconds.sub(
 				Decimal.sumGeometricSeries(
 					Decimal.affordGeometricSeries(data.seconds,this.getCost(),this.multi)
 					,this.getCost(),this.multi)
+			)
+			this.level = this.level.add(
+				Decimal.affordGeometricSeries(prev_seconds,this.getCost(),this.multi)
 			)
 		}
 	}
