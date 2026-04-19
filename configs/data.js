36bca3010fb21a6f8bfb5085a161ccf6e29831c8
Richard Does Stuff <162537734+richardgrechko@users.noreply.github.com>
2026-04-19 21:55:11 +0500

    Update data.js

--- configs/data.js
+++ configs/data.js
@@ -1,3 +1,33 @@
+function sumBase(x,a) {
+    return Decimal.pow(a,x).sub(1).div(Decimal.sub(a,1))
+}
+function revSumBase(x,a) {
+    return Decimal.mul(x,Decimal.sub(a,1)).add(1).log(a).floor()
+}
+
+Decimal.prototype.sumBase = function(a,rev=false) { return rev ? revSumBase(this,a) : sumBase(this,a) }
+
+function powPO(x,b,rev=false) {
+    if (Decimal.lt(b,1.4285714287176406e-8)) {
+        return rev ? Decimal.ln(x).div(b) : Decimal.mul(x,b).exp();
+    } else {
+        return rev ? Decimal.log(x,Decimal.add(b,1)) : Decimal.add(b,1).pow(x);
+    }
+}
+
+Decimal.prototype.powPO = function(x,rev) { return powPO(this,x,rev) }
+
+function sumBasePO(x,a,rev=false) {
+    if (Decimal.lte(a,0)) return x
+    return rev ? Decimal.mul(x,a).add(1).powPO(a,true) : powPO(x,a).sub(1).div(a)
+}
+
+Decimal.prototype.sumBasePO = function(x,rev) { return sumBasePO(this,x,rev) }
+
+const plr = {
+	currentTab: "Main",
+	
+}
 const data = {
 	seconds: new Decimal(0),
 	speed: new Decimal(1e-30), // xNUM speed means you get +NUM seconds per second
@@ -6,19 +36,6 @@ const data = {
 	energyDim: {
 		value: new Decimal(0),
 		amount: new Decimal(0),
-		ups: {
-			X1: {
-				level: new Decimal(0),
-				cost: new Decimal(10),
-				multi: new Decimal(1.5),
-				get totalCost() {
-					return this.cost.mul(this.multi.pow(this.level))
-				},
-				get bulk() {
-					return this.cost.mul(this.multi.pow(this.level))
-				},
-			}
-		},
 	},
 	upgrades: {
 		hastener: new Upgrade({
@@ -41,6 +58,34 @@ const data = {
 			multi: new Decimal(600),
 			gain: new Decimal(4)
 		}),
+		B1: {
+			level: new Decimal(0),
+			cost: new Decimal(20),
+			get totalCost() {
+				return Decimal.pow(7,this.level.sumBasePO(0.1)).mul(this.cost)
+			},
+			get bulk() {
+				return this.level.div(this.cost).log(7).sumBasePO(0.1,true).floor().add(1)
+			},
+			buy() {
+				this.level = this.level.add(1);
+				
+			},
+			curr: "energy"
+		},
+		B2: {
+			level: new Decimal(0),
+			cost: new Decimal(1e50),
+			get totalCost() {
+				return Decimal.pow(30,this.level.sumBasePO(0.1)).mul(this.cost)
+			},
+			get bulk() {
+				return this.level.div(this.cost).log(30).sumBasePO(0.1,true).floor().add(1)
+			},
+			buy() {
+			},
+			curr: "seconds"
+		},
 	},
 	timeResets: {
 		points: new Decimal(0),
